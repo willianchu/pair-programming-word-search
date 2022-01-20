bcd221cd49d5891e2f1ed3ec7b869a61c8a1a475
Willian Chu <48540980+willianchu@users.noreply.github.com>
2022-01-20 02:47:26 -0800

    Pair programming
    @Jiani @willianchu

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,14 +15,30 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present vertically", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'J', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'I', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'A', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'N', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'I', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JIANI');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'H', 'U', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
@@ -31,8 +47,30 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'CHU');
 
     assert.isTrue(result);
+  });
+
+  it("should return undefined if the array is empty", function() {
+    const result = wordSearch([], 'SEINFELD');
+
+    assert.isUndefined(result);
+  });
+
+  it("should return undefined if the word is empty", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'H', 'U', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.isUndefined(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//transpose matrix function @Jiane @WillianChu
+const transpose = function(matrix) {
+    const newMatrix = [];
 
-module.exports = wordSearch
+    for (let i = 0; i < matrix[0].length; i++) {
+        newMatrix.push([]);
+        for (let j = 0; j < matrix.length; j++) {
+        newMatrix[i].push(matrix[j][i]);
+        }
+    }
+    return newMatrix;
+    };
+  
+  //wordSearch function
+  const wordSearch = (letters, word) => {
+    if (letters.length === 0 || word === '') return undefined;
+  
+    //check for horizontal match
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    //rotate the array with transpose()
+    const transposedLetters = transpose(letters);
+        
+    //check for vertical match
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+      
+    return false;
+  };
+  
+  module.exports = wordSearch;
